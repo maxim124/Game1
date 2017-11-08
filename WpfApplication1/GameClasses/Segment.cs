c70673fd22e05378929e11efe7ea9fe0c856dd76
S-Ivanov <s.v.ivanov@bk.ru>
2017-11-08 20:38:02 +0300

    08.11.2017 - промежуточная фиксация - 2

--- WpfApplication1/GameClasses/Segment.cs
+++ WpfApplication1/GameClasses/Segment.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WpfApplication1.GameClasses
+﻿namespace WpfApplication1.GameClasses
 {
+    /// <summary>
+    /// Отрезок (дороги)
+    /// </summary>
     public class Segment
     {
         /// <summary>
-        /// смещение от 0, м
+        /// Смещение от 0, м
         /// </summary>
         public double Offset { get; set; }
 
-        // + длина, м
+        /// <summary>
+        /// Длина, м
+        /// </summary>
         public double Length { get; set; }
     }
 }
